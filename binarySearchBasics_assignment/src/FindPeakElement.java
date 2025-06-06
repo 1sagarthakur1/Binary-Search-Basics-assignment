3e2038d732dab8e31abd869c4dd1abbd92c89fc0
1sagarthakur1 <1sagarthakur1@gmail.com>
2025-06-06 09:36:26 +0530

    add code

---
+++ binarySearchBasics_assignment/src/FindPeakElement.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/find-peak-element/submissions/1655282135/
+
+public class FindPeakElement {
+    public static void main(String[] args) {
+
+        int[] nums =  {1,2,3,1};
+        FindPeakElement findPeakElement = new FindPeakElement();
+        System.out.println(findPeakElement.findPeakElement(nums));
+    }
+
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left < right){
+            int mid = left + (right-left)/2;
+            if(nums[mid] < nums[mid+1]){
+                left=mid+1;
+            }else{
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
